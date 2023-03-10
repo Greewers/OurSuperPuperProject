45b8cce9e9ba4fe83ed4896eb30a031829e74a4c
DESKTOP-36GI5A5\Никита <grincko.nick@yandex.ru>
2023-03-10 14:51:12 +0300

    Убран GameManager, UnitManager и ScriptbleUnits. Исправлена генерация сетки и добавлена настройка частоты генерации стен на сетке

--- Assets/Managers/GridManager.cs
+++ Assets/Managers/GridManager.cs
@@ -4,27 +4,26 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
-    public static GridManager instance;
-
     [SerializeField] private int _wight, _height;
     [SerializeField] private Tile _floorTile, _wallTile;
     [SerializeField] private Transform _cam;
+    [SerializeField] private int _frequency;
 
     private Dictionary<Vector2, Tile> _tiles;
 
-    private void Awake()
+    private void Start()
     {
-        instance = this;
+        GenerateGrid();
     }
-
-    public void GenerateGrid()
+    void GenerateGrid()
     {
+
         _tiles = new Dictionary<Vector2, Tile>();
         for (int x = 0; x < _wight; x++)
         {
             for (int y = 0; y < _height; y++)
             {
-                var randomTile = Random.Range(0,10) ==   3 ? _wallTile : _floorTile; 
+                var randomTile = Random.Range(0, _frequency) ==   0 ? _wallTile : _floorTile; 
                 var spawnedTile = Instantiate(randomTile, new Vector3(x,y), Quaternion.identity);
                 spawnedTile.name = $"Tile {x} {y}";
 
@@ -36,8 +35,6 @@ public class GridManager : MonoBehaviour
         }
 
         _cam.transform.position = new Vector3((float)_wight/2 -0.5f, (float)_height/ 2 - 0.5f,-10);
-
-        GameManager.instance.ChangeState(GameState.SpawnHero);
     }
 
     public Tile GetTileAtPosition(Vector2 pos)
