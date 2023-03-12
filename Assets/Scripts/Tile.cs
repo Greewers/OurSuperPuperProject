dfdc3188b301b3c1d152276be47125101242fbb8
WIN-SGS3JKQFR5K\User <KirSeld22@yandex.ru>
2023-03-12 15:00:00 +0500

    Добавлен ObjectSpawner

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -6,6 +6,12 @@ public abstract class Tile : MonoBehaviour
 {
     [SerializeField] protected SpriteRenderer _renderer;
     [SerializeField] private GameObject _highlight;
+    [SerializeField] private bool _isWalkable; // задает плитке значение проходимости
+
+    public BaseObjects OccupiedObject; 
+    public bool Walkable => _isWalkable && OccupiedObject == null; //определеяет финальную проходимость (проходимость клетке и занята ли объектом)
+
+
 
     public virtual void Init(int x, int y)
     {
